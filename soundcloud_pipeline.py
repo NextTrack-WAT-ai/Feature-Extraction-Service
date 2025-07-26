7a24828a97fb8ca7b3ae7a7bb084033d9219a358
Nathan Chu <nathan.ch.chu@gmail.com>
2025-07-26 00:08:19 -0400

    reverted download options

--- soundcloud_pipeline.py
+++ soundcloud_pipeline.py
@@ -342,8 +342,7 @@ class YTDLPDownloader:
         try:
             # Define full download options with finalized output template
             ydl_opts_download = {
-                'format': 'bestaudio[filesize>10M]/bestaudio/best',
-                'prefer_free_formats': False,
+                'format': 'bestaudio/best',
                 # Use the finalized path (without extension, yt-dlp adds it)
                 'outtmpl': str(final_filename_path.with_suffix('.%(ext)s')),
                 'postprocessors': [{
